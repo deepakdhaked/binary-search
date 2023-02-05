b977c47bf9b675c68c6ce6a2224195a017020a47
deepak dhaked <deepakdhakedpc@gmail.com>
2023-02-05 16:51:39 +0530

    total number of occurance of key is added

---
+++ total-number-of-occourance-of-key.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int firstOccurance(vector<int> &v, int key)
+{
+    int start = 0, end = v.size() - 1;
+    int mid = start + (end - start) / 2;
+    int ans = -1;
+
+    while (start <= end)
+    {
+        if (key == v[mid])
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else if (key > v[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return ans;
+}
+
+int lastOccurance(vector<int> v, int key)
+{
+    int start = 0, end = v.size() - 1;
+    int mid = start + (end - start) / 2;
+    int ans = -1;
+
+    while (start <= end)
+    {
+        if (key == v[mid])
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else if (key > v[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8};
+    int key = 4;
+
+    int firstIndex = firstOccurance(v, key);
+    int lastIndex = lastOccurance(v, key);
+
+    cout << "Total number of occurance of " << key << " is : " << (lastIndex - firstIndex) + 1;
+}
